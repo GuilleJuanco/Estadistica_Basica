77f491e251136896a9ad62efd680615321354fa6
GuilleJuanco <ucancallmeg.github@gmail.com>
2023-07-24 22:34:11 +0200

    Ejercicio 2.2

--- cuaderno.py
+++ cuaderno.py
@@ -5,20 +5,25 @@ import math
 import scipy.stats as st
 import matplotlib.pyplot as plt
 
-#2.1
+#2.2
 
 #Crea DataFrame
-porcentajes={'Suiza': 0.30, 'España': 0.15, 'Italia': 0.10, 'Francia': 0.20, 'Austria': 0.10, 'Alemania': 0.10, 'Otro': 0.05}
-df=pd.DataFrame([porcentajes]).T
+iva={'España': 0.16, 'Italia': 0.20, 'Belgica': 0.06, 'Holanda': 0.06, 'Alemania': 0.07, 'Portugal': 0.17, 'Luxemburgo': 0.06, 'Finlandia': 0.22}
+df=pd.DataFrame([iva]).T
 df.reset_index(inplace=True)
-df.columns = ['pais', 'porcentaje']
+df.columns = ['pais', 'iva']
 #Analizar descriptivamente los datos
 medidas=df.describe().T #media, desviación estandar, min, max, cuantiles.
-recorrido=df['porcentaje'].max() - df['porcentaje'].min()
-media=df['porcentaje'].mean()
-moda=df['porcentaje'].mode()[0]
-mediana=df['porcentaje'].median()
-varianza=df['porcentaje'].var()
+recorrido=df['iva'].max() - df['iva'].min()
+media=df['iva'].mean()
+moda=df['iva'].mode()[0]
+mediana=df['iva'].median()
+'''En el ejercicio anterior hemos determinado como Varianza la Cuasivarianza 
+ya que NumPy así lo establece, esta vez calcularemos la varianza y desviación
+típica manualmente'''
+diferencia_cuadrados = [(x - media) ** 2 for x in df['iva']]
+sumatoria_cuadrados = sum(diferencia_cuadrados)
+varianza = sumatoria_cuadrados / len(df['iva'])
 stddev=math.sqrt(varianza)
 #Variación de Pearson
 Vp = (stddev / media) * 100
--- estadistica_descriptiva.py
+++ estadistica_descriptiva.py
@@ -175,8 +175,37 @@ Af = (media - mediana) / stddev
 print(medidas)
 print("Recorrido:", recorrido,'Media:', media, "Moda:", moda, 'Mediana:', mediana, 'Varianza:', varianza, 'Desviación estandar:', stddev, 'Vp:', Vp, 'Ap:', Ap, 'Af:', Af)
 
-
 #2.2
+
+#Crea DataFrame
+iva={'España': 0.16, 'Italia': 0.20, 'Belgica': 0.06, 'Holanda': 0.06, 'Alemania': 0.07, 'Portugal': 0.17, 'Luxemburgo': 0.06, 'Finlandia': 0.22}
+df=pd.DataFrame([iva]).T
+df.reset_index(inplace=True)
+df.columns = ['pais', 'iva']
+#Analizar descriptivamente los datos
+medidas=df.describe().T #media, desviación estandar, min, max, cuantiles.
+recorrido=df['iva'].max() - df['iva'].min()
+media=df['iva'].mean()
+moda=df['iva'].mode()[0]
+mediana=df['iva'].median()
+'''En el ejercicio anterior hemos determinado como Varianza la Cuasivarianza 
+ya que NumPy así lo establece, esta vez calcularemos la varianza y desviación
+típica manualmente'''
+diferencia_cuadrados = [(x - media) ** 2 for x in df['iva']]
+sumatoria_cuadrados = sum(diferencia_cuadrados)
+varianza = sumatoria_cuadrados / len(df['iva'])
+stddev=math.sqrt(varianza)
+#Variación de Pearson
+Vp = (stddev / media) * 100
+#Asimetría de Pearson
+Ap = (media - moda) / stddev
+#Asimetría de Fisher
+Af = (media - mediana) / stddev
+#Resultados
+print(medidas)
+print("Recorrido:", recorrido,'Media:', media, "Moda:", moda, 'Mediana:', mediana, 'Varianza:', varianza, 'Desviación estandar:', stddev, 'Vp:', Vp, 'Ap:', Ap, 'Af:', Af)
+
+
 
 #2.3
 
