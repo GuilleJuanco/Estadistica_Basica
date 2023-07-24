222195cff491f41c719939657ea7b0ecb06bbff5
GuilleJuanco <ucancallmeg.github@gmail.com>
2023-07-24 15:10:43 +0200

    Ejemplo 2.9 pag.80

--- estadistica_descriptiva.py
+++ estadistica_descriptiva.py
@@ -117,14 +117,31 @@ plt.show()
 
 #Nube de puntos del ejemplo 2.9 pag.77
 
-#Crea listas con datos
+# Crea listas con datos
 horas=[21, 32, 15, 40, 27, 18, 26, 50, 33, 51, 36, 16, 19, 22, 16, 39, 56, 29, 45, 25]
 marca=[13.2, 12.6, 13, 12.2, 15, 14.8, 14.8, 12.2, 13.6, 12.6, 13.1, 14.9, 13.9, 13.2, 15.1, 14.1, 13, 13.5, 12.7, 14.2]
-
-#Crea gráfico
+# Crea gráfico
 plt.scatter(horas, marca)
 plt.xlabel('Horas')
 plt.ylabel('Marca')
 plt.title('Nube de puntos')
+plt.grid(True)
+plt.show()
+
+# Calcular regresión lineal ejemplo 2.9 pag.80
+
+horas = [21, 32, 15, 40, 27, 18, 26, 50, 33, 51, 36, 16, 19, 22, 16, 39, 56, 29, 45, 25]
+marca = [13.2, 12.6, 13, 12.2, 15, 14.8, 14.8, 12.2, 13.6, 12.6, 13.1, 14.9, 13.9, 13.2, 15.1, 14.1, 13, 13.5, 12.7, 14.2]
+#Calcula coeficientes regresión lineal
+coeficientes = np.polyfit(horas, marca, 1)
+b1, b0 = coeficientes
+#Crea gráfico
+plt.scatter(horas, marca, label='Entradas de Datos')
+plt.xlabel('Horas')
+plt.ylabel('Marca')
+plt.title('Nube de puntos con linea de Regresión Lineal')
+#Añade linea de regresión lineal
+plt.plot(horas, b1 * np.array(horas) + b0, color='red', label='Regresión Lineal')
+plt.legend()
 plt.grid(True)
 plt.show()
