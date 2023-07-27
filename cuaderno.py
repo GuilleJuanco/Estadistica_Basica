91bb769000f1c18e344247623ce81479a71b49a4
GuilleJuanco <ucancallmeg.github@gmail.com>
2023-07-27 14:24:45 +0200

    Ejercicio 2.5

--- cuaderno.py
+++ cuaderno.py
@@ -5,29 +5,29 @@ import math
 import scipy.stats as st
 import matplotlib.pyplot as plt
 
-#2.4
+#2.5
 
 #Crea DataFrame
-circunferencia=[68,100, 85, 133, 130, 165, 120, 120, 155, 117]
-peso=[6, 6.3, 6, 6.3, 7, 8, 7, 8.1, 9, 6.6]
-data={'circunferencia_cm': circunferencia, 'peso_gramos': peso}
+bebe=[160, 180, 300, 380]
+adulto=[400, 450, 800, 1350]
+data={'volumen_bebe': bebe, 'volumen_adulto': adulto}
 df=pd.DataFrame(data)
 #Calcular coeficientes regresión lineal manualmente.
-n = len(df['peso_gramos'])
-xy_sum = (df['peso_gramos'] * df['circunferencia_cm']).sum()
-x_sum = df['circunferencia_cm'].sum()
-y_sum = df['peso_gramos'].sum()
-x_squared_sum = (df['circunferencia_cm'] ** 2).sum()
+n = len(df['volumen_adulto'])
+xy_sum = (df['volumen_adulto'] * df['volumen_bebe']).sum()
+x_sum = df['volumen_bebe'].sum()
+y_sum = df['volumen_adulto'].sum()
+x_squared_sum = (df['volumen_bebe'] ** 2).sum()
 b1 = (n * xy_sum - x_sum * y_sum) / (n * x_squared_sum - x_sum ** 2)
 b0 = (y_sum - b1 * x_sum) / n
 #Calcular coeficientes regresión lineal usando NumPy
-coeficientes = np.polyfit(circunferencia, peso, 1)
+coeficientes = np.polyfit(bebe, adulto, 1)
 m, b = coeficientes
 #Calcular Coeficiente de correlación lineal de Pearson
-media_x = np.mean(circunferencia)
-media_y = np.mean(peso)
-dif_x = np.array(circunferencia) - media_x
-dif_y = np.array(peso) - media_y
+media_x = np.mean(bebe)
+media_y = np.mean(adulto)
+dif_x = np.array(bebe) - media_x
+dif_y = np.array(adulto) - media_y
 #Numerador
 numerador = np.sum(dif_x * dif_y)
 #Denominador
--- estadistica_descriptiva.py
+++ estadistica_descriptiva.py
@@ -284,3 +284,32 @@ denominador_y = np.sqrt(np.sum(dif_y ** 2))
 r = numerador / (denominador_x * denominador_y)
 
 #2.5
+
+#Crea DataFrame
+bebe=[160, 180, 300, 380]
+adulto=[400, 450, 800, 1350]
+data={'volumen_bebe': bebe, 'volumen_adulto': adulto}
+df=pd.DataFrame(data)
+#Calcular coeficientes regresión lineal manualmente.
+n = len(df['volumen_adulto'])
+xy_sum = (df['volumen_adulto'] * df['volumen_bebe']).sum()
+x_sum = df['volumen_bebe'].sum()
+y_sum = df['volumen_adulto'].sum()
+x_squared_sum = (df['volumen_bebe'] ** 2).sum()
+b1 = (n * xy_sum - x_sum * y_sum) / (n * x_squared_sum - x_sum ** 2)
+b0 = (y_sum - b1 * x_sum) / n
+#Calcular coeficientes regresión lineal usando NumPy
+coeficientes = np.polyfit(bebe, adulto, 1)
+m, b = coeficientes
+#Calcular Coeficiente de correlación lineal de Pearson
+media_x = np.mean(bebe)
+media_y = np.mean(adulto)
+dif_x = np.array(bebe) - media_x
+dif_y = np.array(adulto) - media_y
+#Numerador
+numerador = np.sum(dif_x * dif_y)
+#Denominador
+denominador_x = np.sqrt(np.sum(dif_x ** 2))
+denominador_y = np.sqrt(np.sum(dif_y ** 2))
+#Calcula r
+r = numerador / (denominador_x * denominador_y)
