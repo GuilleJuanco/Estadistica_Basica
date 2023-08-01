39ae38b8b23800be676b0332d89da07cb9799ddd
GuilleJuanco <ucancallmeg.github@gmail.com>
2023-08-01 23:22:44 +0200

    Ejemplo 3.10

--- cuaderno.py
+++ cuaderno.py
@@ -5,34 +5,6 @@ import math
 import scipy.stats as st
 import matplotlib.pyplot as plt
 
-#2.5
-
-#Crea DataFrame
-bebe=[160, 180, 300, 380]
-adulto=[400, 450, 800, 1350]
-data={'volumen_bebe': bebe, 'volumen_adulto': adulto}
-df=pd.DataFrame(data)
-#Calcular coeficientes regresión lineal manualmente.
-n = len(df['volumen_adulto'])
-xy_sum = (df['volumen_adulto'] * df['volumen_bebe']).sum()
-x_sum = df['volumen_bebe'].sum()
-y_sum = df['volumen_adulto'].sum()
-x_squared_sum = (df['volumen_bebe'] ** 2).sum()
-b1 = (n * xy_sum - x_sum * y_sum) / (n * x_squared_sum - x_sum ** 2)
-b0 = (y_sum - b1 * x_sum) / n
-#Calcular coeficientes regresión lineal usando NumPy
-coeficientes = np.polyfit(bebe, adulto, 1)
-m, b = coeficientes
-#Calcular Coeficiente de correlación lineal de Pearson
-media_x = np.mean(bebe)
-media_y = np.mean(adulto)
-dif_x = np.array(bebe) - media_x
-dif_y = np.array(adulto) - media_y
-#Numerador
-numerador = np.sum(dif_x * dif_y)
-#Denominador
-denominador_x = np.sqrt(np.sum(dif_x ** 2))
-denominador_y = np.sqrt(np.sum(dif_y ** 2))
-#Calcula r
-r = numerador / (denominador_x * denominador_y)
-
+#Ejemplo 3.10 pag.102.
+dic={'a': [0.3, 0.2], 'b': [0.1, 0.4], 'c': [0.6, 0.05]}
+prob=dic['a'][0] * dic['a'][1] + dic['b'][0] * dic['b'][1] + dic['c'][0] * dic['c'][1]
--- probabilidad.py
+++ probabilidad.py
@@ -1,3 +1,14 @@
+# Importa librerías
+import pandas as pd
+import numpy as np
+import math
+import scipy.stats as st
+import matplotlib.pyplot as plt
+
 #Ejemplo 3.3 pag.96.
 dic={'azules': [0.05], 'verdes': [0.02], 'castaños': [0.69], 'negros': [0.24]}
 prob=dic['azules'][0] + dic['verdes'][0]
+
+#Ejemplo 3.10 pag.102.
+dic={'a': [0.3, 0.2], 'b': [0.1, 0.4], 'c': [0.6, 0.05]}
+prob=dic['a'][0] * dic['a'][1] + dic['b'][0] * dic['b'][1] + dic['c'][0] * dic['c'][1]
